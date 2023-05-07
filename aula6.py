060ef5a34afe96d3108e41706abc53cd058a4c80
jhonnatas <jhonnatas.aires@gmail.com>
2023-05-06 21:18:58 -0300

    learning elif

---
+++ aula6.py
@@ -0,0 +1,20 @@
+condicao = True
+a = 10
+b = 5
+c = 3
+
+if condicao:
+  print('executou')
+else:
+  print('Não executou')
+  
+if a < b:
+  print('faca isso')
+elif a < b:
+  print('faca aquilo')
+elif a > c:
+  print('faca aquilo outro')
+elif a > c:
+  print('faca aquilo mais')
+
+# Aprendendo elif
